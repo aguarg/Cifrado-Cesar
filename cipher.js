9188b5efb49299d41680f6af0caad38617bb4146
huagru <prosopisnigra@gmail.com>
2021-11-24 11:35:13 -0300

    Cifrado césar completo

--- cipher.js
+++ cipher.js
@@ -4,7 +4,7 @@ Javascript:
 ENTER, porque el ENTER es para dejar un renglón en la caja y poder seguir escribiendo.
 */
 
-/*Los caracteres capaz de leer y descifrar:*/
+/*Los caracteres capaz de leer y cifrar/descifrar:*/
 const abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "á", 
 "é", "í", "ó", "ú", ",", ".", "?", "¿", "!", "¡", ";", "1", "2", "3", "4", "5",
